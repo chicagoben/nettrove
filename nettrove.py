ab8b9d4f2801219cb4940195d57689553cc31c23
Benjamin Johnson <benjaminjohnson80@gmail.com>
2011-01-07 19:15:49 -0800

    initial skeleton

---
+++ nettrove.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""The Nettrove Communications Library."""
+
+version = "0.1"
+version_info = (0, 1, 0)
+
+
+class nt_endpoint ( object ) :
+    def __init__() :
+        pass
+
+class nt_inbound ( nt_endpoint ) :
+    def __init__() :
+        pass
+
+class nt_outbound ( nt_endpoint ) :
+    def __init__() :
+        pass
+
+class msg_requester ( object ) :
+    def __init__() :
+        pass
+
+class msg_responder ( object ) :
+    def __init__() :
+        pass
+
+class msg_publisher ( object ) :
+    def __init__() :
+        pass
+
+class msg_subscriber ( object ) :
+    def __init__() :
+        pass
+
+class feed_consumer ( object ) :
+    def __init__() :
+        pass
+
+class feed_producer ( object ) :
+    def __init__() :
+        pass
+
+
+if __name__ == "__main__" :
+    pass
+
